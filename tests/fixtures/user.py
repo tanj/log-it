88cda9521a260fd982579b802659ddd1160cde9a
John teBokkel <tanj.tanj@gmail.com>
2021-01-03 11:46:03 -0800

    user fixture deletes itself when finished

--- tests/fixtures/user.py
+++ tests/fixtures/user.py
@@ -17,9 +17,11 @@ from log_it.user.model import TUser
 @pytest.fixture
 def user():
     """Creates a normal user"""
-    return TUser.create(
+    user = TUser.create(
         sEmail="test_normal@example.com",
         sName="Normal Test",
         urlProfilePic="http://example.com/user/test_normal/profile_pic.png",
         urlDefaultLog="log/test_normal_at_example_com",
     )
+    yield user
+    user.delete()
