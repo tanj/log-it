d0026eb760e43488738d1f4dc6106ae29e40f672
John teBokkel <tanj.tanj@gmail.com>
2021-01-02 21:12:45 -0800

    add log models

---
+++ log_it/log/model.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+    log_it.log.model
+    ----------------
+
+    It provides the models for the log
+
+    :copyright: (c) 2020 by John te Bokkel
+    :license: BSD, see LICENSE for more details
+
+"""
+from datetime import datetime
+
+from sqlalchemy_utils.models import generic_repr
+
+from log_it.extensions import db
+from log_it.utils.database import Timestamp, CRUDMixin
+
+
+@generic_repr
+class TLog(db.Model, CRUDMixin, Timestamp):
+    __tablename__ = "tLog"
+
+    ixLog = db.Column(db.Integer, primary_key=True)
+    sLog = db.Column(db.Unicode)
+    ixUser = db.Column(db.Integer, db.ForeignKey("tUser.ixUser"), nullable=False)
+    fields = db.relationship("TLogField", uselist=True)
+
+
+# TODO: log sharing
+# @generic_repr
+# class TSharedLog(db.Model, CRUDMixin, Timestamp):
+#     __tablename__ = "tSharedLog"
+
+#     ixSharedLog = db.Column(db.Integer, primary_key=True)
+#     ixLog = db.Column(db.Integer, db.ForeignKey("tLog.ixLog"), nullable=False)
+#     permissions = db.relationship("TPermission", uselist=True)
+#     # This should be it's own table
+#     ixUser = db.Column(db.Integer, db.ForeignKey("tUser.ixUser"), nullable=False)
+#     pemUser = db.Column(db.Integer, nullable=False)
+#     ixGroup = db.Column(db.Integer, db.ForeignKey(), nullable=True)
+#     pemGroup = db.Column(db.Integer, nullable=True)
+
+
+@generic_repr
+class TField(db.Model, CRUDMixin):
+    __tablename__ = "tField"
+
+    ixField = db.Column(db.Integer, primary_key=True)
+    sField = db.Column(db.Unicode)
+
+
+@generic_repr
+class TLogField(db.Model, CRUDMixin):
+    __tablename__ = "tLogField"
+
+    ixField = db.Column(db.Integer, db.ForeignKey("tField.ixField"), primary_key=True)
+    field = db.relationship("TField", uselist=False)
+    ixLog = db.Column(db.Integer, db.ForeignKey("tLog.ixLog"), primary_key=True)
+    log = db.relationship("TLog", uselist=False)
+    sValue = db.Column(db.Unicode)
+    iOrder = db.Column(db.Integer)
+
+
+@generic_repr
+class TMessage(db.Model, CRUDMixin, Timestamp):
+    __tablename__ = "tMessage"
+
+    ixMessage = db.Column(db.Integer, primary_key=True)
+    ixLog = db.Column(
+        db.Integer, db.ForeignKey("tLog.ixLog", ondelete="CASCADE"), nullable=False
+    )
+    log = db.relationship("TLog", uselist=False)
+    ixMessageType = db.Column(
+        db.Integer, db.ForeignKey("tMessageType.ixMessageType"), nullable=False
+    )
+    message_type = db.relationship("TMessageType", uselist=False)
+    # utcMessage is added for being able to change some history if a note was missed
+    utcMessage = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    ixUser = db.Column(
+        db.Integer, db.ForeignKey("tUser.ixUser", ondelete="CASCADE"), nullable=False
+    )
+    user = db.relationship("TUser", uselist=False)
+
+
+@generic_repr
+class TMessageType(db.Model, CRUDMixin):
+    __tablename__ = "tMessageType"
+
+    ixMessageType = db.Column(db.Integer, primary_key=True)
+    sMessageType = db.Column(db.Unicode(20), unique=True)
+
+
+@generic_repr
+class TTag(db.Model, CRUDMixin):
+    __tablename__ = "tTag"
+
+    ixTag = db.Column(db.Integer, primary_key=True)
+    sTag = db.Column(db.Unicode(40), nullable=False)
+
+
+@generic_repr
+class TTagMessage(db.Model, CRUDMixin):
+    __tablename__ = "tTagMessage"
+
+    ixMessage = db.Column(
+        db.Integer, db.ForeignKey("tMessage.ixMessage"), primary_key=True
+    )
+    ixTag = db.Column(db.Integer, db.ForeignKey("tTag.ixTag"), primary_key=True)
+
+
+_indexes = [
+    db.Index("uq_ttag_stag_lower", db.func.lower(TTag.sTag), unique=True),
+]
--- log_it/utils/database.py
+++ log_it/utils/database.py
@@ -13,6 +13,7 @@ from datetime import datetime
 
 import sqlachemy as sa
 from sqlalchemy.ext.declarative import declared_attr
+from log_it.extensions import db
 
 
 def last_position(cls):
@@ -51,3 +52,52 @@ class Timestamp(object):
 def timestamp_before_update(mapper, connection, target):
     """Listen for `before_update` event and set `utcUpdated`"""
     target.utcUpdated = datetime.utcnow()
+
+
+class CRUDMixin(object):
+    """
+    CRUDMixin copied and adapted from FlaskBB under
+    the terms of their BSD license
+    :copyright: (c) 2014 by the FlaskBB Team.
+    :license: BSD, see LICENSE for more details.
+    :source: https://github.com/flaskbb/flaskbb
+    """
+
+    @classmethod
+    def create(cls, commit=True, **kwargs):
+        """Create a `cls` object from kwargs
+
+        :param cls: The class calling this method
+        :param commit: whether we commit on creation or just add to session
+        :returns: instance of `cls`
+        :rtype: `cls`
+
+        """
+        instance = cls(**kwargs)
+        return instance.save(commit)
+
+    def save(self, commit=True):
+        """Saves the object to the database."""
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self):
+        """Delete the object from the database."""
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def pk_to_dict(self):
+        """:property pk_to_dict:
+
+        :returns: primary keys as dictionary
+        :rtype: Mapping[pk_name, pk_value]
+
+        """
+        pkd = {}
+        for col in self.__table__.primary_key.columns:
+            pkd[col.name] = getattr(self, col.name)
+        return pkd
